f92f134966d98a7750ff577fe36a6db46c4ab861
dairedo7 <vladik719@gmail.com>
2023-01-24 21:01:10 +0100

    bookreading_app_files

---
+++ src/API.js
@@ -0,0 +1,1 @@
+export const baseUrl = 'https://bookreading-backend.onrender.com/';
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.min.css';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
+    </Provider>
+);
